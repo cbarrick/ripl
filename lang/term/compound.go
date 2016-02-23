9b5425777ca4b6a87a675fa5581a0037a4d03a15
Chris Barrick <cbarrick1@gmail.com>
2016-02-23 11:48:42 -0500

    Add breadth-first traversal of compounds

--- lang/term/compound.go
+++ lang/term/compound.go
@@ -22,3 +22,40 @@ func (c Compound) String() (str string) {
 	}
 	return str
 }
+
+func (c Compound) BreadthFirst(f func(int, Term)) {
+	var i int
+	ch := make(chan Term)
+	go c.bfs(ch)
+	for t := range ch {
+		f(i, t)
+		i++
+	}
+}
+
+func (c Compound) bfs(ch chan Term) {
+	ch <- c
+	subs := make([]chan Term, 0, len(c.Args))
+	for _, arg := range c.Args {
+		if comp, ok := arg.(Compound); ok {
+			sub := make(chan Term)
+			subs = append(subs, sub)
+			go comp.bfs(sub)
+			ch <- <-sub
+		} else {
+			ch <- arg
+		}
+	}
+	i := 0
+	for len(subs) > 0 {
+		val := <-subs[i]
+		if val == nil {
+			subs = append(subs[:i], subs[i+1:]...)
+		} else {
+			ch <- val
+			i += 1
+			i %= len(subs)
+		}
+	}
+	close(ch)
+}
